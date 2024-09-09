e4a769e565c3a7a704af44ba1c2012309c1197b1
theek23 <theekshanadesilva28@gmail.com>
2024-09-09 19:29:45 +0530

    hero page and 2nd section created. need to update hero page image with gradient color.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="mb-4 flex justify-center flex-col items-center">
             <img class="max-w-36" src="{{ asset('images/logo.webp') }}" alt="HypeDeck">
             <h1 class="text-4xl font-semibold font-sans ">Let's begin creating something amazing</h1>
-            <p class="mt-2 font-normal font-sans text-base">Join over 500+ startups already growing with Hypedeck Digital</p>
+            <p class="mt-2 font-normal font-sans text-base">Join over 500+ startups already growing with HypeDeck Digital</p>
             <button class="mt-10 px-6 py-2 bg-blue-50/15 text-white rounded-md font-semibold font-sans border hover:bg-blue-500 hover:text-white hover:border-transparent">
                 Book Free Consultation
             </button>
@@ -12,22 +12,24 @@
         <p class="mt-8 font-sans">Office - 2/1, 1st Floor, 272B High Level Rd, Maharagama</p>
     </div>
 
-    <div class="w-full mx-auto max-w-screen-xl p-4 flex flex-wrap flex-col justify-center sm:justify-between sm:flex-row bg-blue-50/10">
-        <div class="text-sm text-white sm:text-center font-sans">© 2023
-            <a href="#" class="hover:underline">HypeDeck</a>. All Rights Reserved.
-        </div>
-        <div class="flex justify-center items-center text-sm text-white sm:text-center font-sans">
-            <ul class="flex flex-row mt-3 text-sm sm:mt-0">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6 font-sans">Terms</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6 font-sans">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6 font-sans">Cookies</a>
-                </li>
-            </ul>
+    <div class="min-w-full mx-auto max-w-screen-xl p-4  bg-blue-50/10">
+        <div class="px-14 w-full flex flex-wrap flex-col justify-center sm:justify-between sm:flex-row">
+            <div class="text-sm text-white sm:text-center font-sans">© 2023
+                <a href="#" class="hover:underline">HypeDeck</a>. All Rights Reserved.
+            </div>
+            <div class="flex justify-center items-center text-sm text-white sm:text-center font-sans">
+                <ul class="flex flex-row mt-3 text-sm sm:mt-0">
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6 font-sans">Terms</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6 font-sans">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6 font-sans">Cookies</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ export default {
             fontFamily: {
                 sans: ['Red Hat Display', 'sans-serif'],
             },
+            colors: {
+                'custom-gradient-start': '#012F6A',
+                'custom-gradient-end': '#1764DF',
+            },
         },
     },
     plugins: [],
