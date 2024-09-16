d6a720cd71bb9ebb6b78eb999a0f6b08e86e4040
Yuraj Malinda <yurajmalinda123@gmail.com>
2024-09-16 22:23:03 +0530

    FIve step page created.

---
+++ resources/views/components/fivestep.blade.php
@@ -0,0 +1,119 @@
+<section class="flex-1">
+    <div class="relative"
+        style="background: url('{{ asset('images/bg-five-step.webp') }}'); background-repeat: no-repeat; height: 100%; background-size: cover;">
+        <div class="font-sans py-5 lg:pt-10 lg:pb-28 px-3 flex justify-content-center flex-col space-y-8 lg:space-y-16 " style="height:100%;">
+            <span class="text-[1.35rem] sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center text-white font-bold tracking-wider leading-7 lg:leading-[3rem] xl:leading-[4rem]">Our 5 Step Process For
+                a Successful Digital<br> Marketing Campaign</span>
+
+            <section class="relative w-full flex flex-col lg:flex-row lg:px-[2rem] xl:px-[4rem] items-center space-y-4 sm:space-y-3 md:space-y-6 lg:space-y-none lg:space-x-4 xl:space-x-9">
+                <div class="w-full md:w-11/12 lg:w-[55%] xl:w-6/12 space-y-2 md:space-y-3 lg:space-y-3 xl:space-y-4 h-auto">
+
+                    <div id="faq1" class="py-2 px-2 md:py-3 md:px-3 lg:py-3 lg:px-3 xl:py-4 xl:px-4  bg-white text-[#012F6A] rounded-[0.3rem] lg:rounded-[0.4rem] xl:rounded-[0.5rem] shadow-2xl" onclick="openAnsSection(1)">
+                        <div class="flex justify-between items-center cursor-pointer">
+                            <h3 class="font-[600] tracking-wider lg:font-bold text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] leading-5">Market Analysis</h3>
+                            <button aria-label="too" class=" cursor-pointer" onclick="openAnsSection(1)">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path id="path1" class="" d="M10 4.1665V15.8332" stroke="currentColor"
+                                        stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M4.16602 10H15.8327" stroke="currentColor" stroke-width="1.25"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </button>
+                        </div>
+                        <p id="para1" class="text-[0.85rem] text-pretty md:text-[0.9rem] lg:text-[0.9rem] xl:text-[1rem] hidden font-light lg:font-normal text-base leading-5 lg:leading-5 xl:leading-6 mt-2 lg:mt-3 xl:mt-4 w-full">
+                            This includes the background research on the brand, product
+                            and the audience. And also the technical audit on previous
+                            campaigns.</p>
+                    </div>
+
+                    <div id="faq2" class="py-2 px-2 md:py-3 md:px-3 lg:py-3 lg:px-3 xl:py-4 xl:px-4  bg-white text-[#012F6A] rounded-[0.3rem] lg:rounded-[0.4rem] xl:rounded-[0.5rem] shadow-2xl" onclick="openAnsSection(2)">
+                        <div class="flex justify-between items-center cursor-pointer">
+                            <h3 class="font-[600] tracking-wider lg:font-bold text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] leading-5">Planning</h3>
+                            <button aria-label="too" class=" cursor-pointer" onclick="openAnsSection(2)">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path id="path2" class="" d="M10 4.1665V15.8332" stroke="currentColor"
+                                        stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M4.16602 10H15.8327" stroke="currentColor" stroke-width="1.25"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </button>
+                        </div>
+                        <p id="para2" class="text-[0.85rem] text-pretty md:text-[0.9rem] lg:text-[0.9rem] xl:text-[1rem] hidden font-light lg:font-normal text-base leading-5 lg:leading-5 xl:leading-6 mt-2 lg:mt-3 xl:mt-4 w-full">
+                            From the insights and findings from the market analysis we
+                            plan the digital media campaign within your sustainable
+                            budget.</p>
+                    </div>
+
+                    <div id="faq3" class="py-2 px-2 md:py-3 md:px-3 lg:py-3 lg:px-3 xl:py-4 xl:px-4  bg-white text-[#012F6A] rounded-[0.3rem] lg:rounded-[0.4rem] xl:rounded-[0.5rem] shadow-2xl" onclick="openAnsSection(3)">
+                        <div class="flex justify-between items-center cursor-pointer">
+                            <h3 class="font-[600] tracking-wider lg:font-bold text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] leading-5">Assets Development</h3>
+                            <button aria-label="too" class=" cursor-pointer" onclick="openAnsSection(3)">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path id="path3" class="" d="M10 4.1665V15.8332" stroke="currentColor"
+                                        stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M4.16602 10H15.8327" stroke="currentColor" stroke-width="1.25"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </button>
+                        </div>
+                        <p id="para3" class="text-[0.85rem] text-pretty md:text-[0.9rem] lg:text-[0.9rem] xl:text-[1rem] hidden font-light lg:font-normal text-base leading-5 lg:leading-5 xl:leading-6 mt-2 lg:mt-3 xl:mt-4 w-full">
+                            We develop artworks, videos, copies, headlines, hooks and
+                            landing pages to create and engage the potential buyer to a
+                            flawless sales journey.</p>
+                    </div>
+                    
+                    <div id="faq4" class="py-2 px-2 md:py-3 md:px-3 lg:py-3 lg:px-3 xl:py-4 xl:px-4  bg-white text-[#012F6A] rounded-[0.3rem] lg:rounded-[0.4rem] xl:rounded-[0.5rem] shadow-2xl" onclick="openAnsSection(4)">
+                        <div class="flex justify-between items-center cursor-pointer">
+                            <h3 class="font-[600] tracking-wider lg:font-bold text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] leading-5">Testing</h3>
+                            <button aria-label="too" class=" cursor-pointer" onclick="openAnsSection(4)">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path id="path4" class="" d="M10 4.1665V15.8332" stroke="currentColor"
+                                        stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M4.16602 10H15.8327" stroke="currentColor" stroke-width="1.25"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </button>
+                        </div>
+                        <p id="para4" class="text-[0.85rem] text-pretty md:text-[0.9rem] lg:text-[0.9rem] xl:text-[1rem] hidden font-light lg:font-normal text-base leading-5 lg:leading-5 xl:leading-6 mt-2 lg:mt-3 xl:mt-4 w-full">
+                            We create different digital media campaigns with different
+                            targeting and different assets to understand the best
+                            working setup.</p>
+                    </div>
+
+                    <div id="faq5" class="py-2 px-2 md:py-3 md:px-3 lg:py-3 lg:px-3 xl:py-4 xl:px-4  bg-white text-[#012F6A] rounded-[0.3rem] lg:rounded-[0.4rem] xl:rounded-[0.5rem] shadow-2xl" onclick="openAnsSection(5)">
+                        <div class="flex justify-between items-center cursor-pointer">
+                            <h3 class="font-[600] tracking-wider lg:font-bold text-[0.9rem] md:text-[1rem] lg:text-[1rem] xl:text-[1.2rem] leading-5">Scaling</h3>
+                            <button aria-label="too" class=" cursor-pointer" onclick="openAnsSection(5)">
+                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path id="path5" class="" d="M10 4.1665V15.8332" stroke="currentColor"
+                                        stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M4.16602 10H15.8327" stroke="currentColor" stroke-width="1.25"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </button>
+                        </div>
+                        <p id="para5" class="text-[0.85rem] text-pretty md:text-[0.9rem] lg:text-[0.9rem] xl:text-[1rem] hidden font-light lg:font-normal text-base leading-5 lg:leading-5 xl:leading-6 mt-2 lg:mt-3 xl:mt-4 w-full">
+                            Once the best working setup is found we scale the campaign.
+                            At this stage we also help the business owner/team to
+                            convert sales efficiently.</p>
+                    </div>
+                </div>
+                <a
+                    class="xl:h-16 lg:h-14 h-12 xl:w-16 lg:w-14 w-12 bg-[#1764DF] flex justify-center items-center rounded-full shadow-2xl">
+                    <img class="lx:h-7 lg:h-6 h-5 -rotate-90 lg:rotate-180" src="{{ asset('images/next.png') }}" alt="next-image">
+                </a>
+                <div class="p-3 md:p-6 lg:p-6 xl:p-8 flex flex-row bg-[#1764DF] text-white rounded-lg items-center space-x-4 sm:space-x-5 md:space-x-6 lg:space-x-5 xl:space-x-7 shadow-2xl">
+                    <span class="font-bold text-3xl md:text-5xl lg:text-5xl font-sans">300%</span>
+                    <span class="font-semibold text-[0.9rem] lg:text-[1rem] text-pretty font-sans">Increase in<br> 
+                        referral traffic</span>
+                </div>
+            </section>
+        </div>
+    </div>
+
+</section>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,19 +1,65 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{env('APP_NAME')}}</title>
+    <title>{{ env('APP_NAME') }}</title>
     @vite(['resources/css/app.css', 'resource/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
-<body>
-<header>
-    <x-header></x-header>
-</header>
 
-<main>
-    <x-webcontent/>
-    <x-footer></x-footer>
-</main>
+<body>
+    <header>
+        <x-header></x-header>
+    </header>
+
+    <main>
+        <x-webcontent />
+        <x-footer></x-footer>
+    </main>
 </body>
+<script>
+    function openAnsSection(val) {
+        // Close all other open FAQs
+        var allParas = document.querySelectorAll('[id^="para"]');
+        var allSvgs = document.querySelectorAll('[id^="path"]');
+        var allFaqs = document.querySelectorAll('[id^="faq"]');
+
+
+        allParas.forEach((para, index) => {
+            if (para.id !== "para" + val) {
+                para.classList.add("hidden");
+                para.classList.remove("block");
+                allSvgs[index].classList.add("block");
+                allSvgs[index].classList.remove("hidden");
+                allFaqs[index].classList.add("bg-white", "text-[#012F6A]");
+                allFaqs[index].classList.remove("bg-[#1764DF]", "text-white");
+            }
+        });
+
+        // Toggle the clicked FAQ
+        var p = document.getElementById("para" + val);
+        var svg = document.getElementById("path" + val);
+        var faq = document.getElementById("faq" + val);
+
+        if (p.classList.contains("hidden")) {
+            // Show answer section and apply active styles
+            p.classList.remove("hidden");
+            p.classList.add("block");
+            svg.classList.remove("block");
+            svg.classList.add("hidden");
+            faq.classList.remove("bg-white", "text-[#012F6A]");
+            faq.classList.add("bg-[#1764DF]", "text-white");
+        }
+    }
+
+    function openFirstFAQ() {
+        openAnsSection(1); // Assuming the first FAQ has val=1
+    }
+
+    // Call openFirstFAQ when the page loads
+    window.onload = openFirstFAQ;
+</script>
+
 </html>
